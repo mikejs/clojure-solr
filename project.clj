7c62fe5c9b777a8b46d12101a3b88171882d9825
Ryan Roemmich <ryan@weareinstrument.com>
2012-06-15 10:16:37 -0700

    fix groupid

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject "0.2.0-SNAPSHOT"
+(defproject clojure-solr "0.2.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.apache.solr/solr-solrj "3.5.0"]
                  [org.slf4j/slf4j-simple "1.6.1"]]
