850872665b96cf9d27ab636b5eabb2671e6182b0
Devin Walters <devinw@gmail.com>
2012-09-29 11:53:06 -0500

    Update dependencies (solrj 3.5.0)

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject clojure-solr "0.2.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+(defproject clojure-solr "1.4.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [org.apache.solr/solr-solrj "1.4.0"]
-                 [org.slf4j/slf4j-simple "1.5.5"]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
+                 [org.apache.solr/solr-solrj "3.5.0"]
+                 [org.slf4j/slf4j-simple "1.6.1"]])
